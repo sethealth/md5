64060f249085bb290d28d1244b99ef07e2cb14f2
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2021-07-13 13:04:09 +0200

    Add test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,10 @@
 
-const {createMd5} = require("../dist/index.js");
-const fs = require("fs");
+const {createMD5} = require("../dist/index.js");
 
-const md5 = createMd5();
-md5.update(new Uint8Array(fs.readFileSync(process.argv[2])));
-console.log(md5.base64());
+const md5 = createMD5();
+md5.update(new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7]));
+const buffer = Buffer.from(md5.digest());
+
+if (buffer.toString("base64") !== "NndQl1HM9hU5F00rljWnvw==") {
+  process.exit(1);
+}
